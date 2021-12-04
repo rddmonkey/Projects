361eb5ba7f78f348eda27f224039edb31a1b0d4a
Richard Chang <76931794+rddmonkey@users.noreply.github.com>
2021-12-03 21:17:48 -0800

    Update leetcode.py
    This update allows you to attach .txt files. It doesn't work for other types....

--- leetcode.py
+++ leetcode.py
@@ -1,24 +1,32 @@
-
 import smtplib
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 from email import message
 
-from_addr = "rddmonkey@gmail.com"
-to_addr = "richd_chang@hotmail.com"
+from_addr = 'rddmonkey@gmail.com'
+to_addr = 'richd_chang@hotmail.com'
+subject = 'I just sent this attachment from Python!'
+content = 'How neat is that?'
 
-subject = "I just sent this from Python!"
-body = "this is so cool"
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = subject
+body = MIMEText(content, 'plain')
+msg.attach(body)
 
-msg = message.Message()
+filename = "test.txt"
 
-msg.add_header('from', from_addr)
-msg.add_header("to",to_addr)
-msg.add_header("subject",subject)
-msg.set_payload(body)
+with open(filename, 'r') as f:
+    part = MIMEApplication(f.read(), Name=basename(filename))
+    part['Content-Disposition'] = 'attachment; filename="{}"'.format(basename(filename))
+    msg.attach(part)
+
+msg.attach(part)
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-
-server.login(from_addr, "password")
-
-server.send_message(msg, from_addr=from_addr, to_addrs = to_addr)
-exit()
+server.login(from_addr,"password")
+server.send_message(msg, from_addr=from_addr, to_addrs=to_addr)
